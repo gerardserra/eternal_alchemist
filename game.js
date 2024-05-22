274a6e9cc24f6941caaa7198f6014d63eeb22e7b
gerardserra <gerardsgs@gmail.com>
2024-05-22 17:55:27 +0200

    test 2

--- game.js
+++ game.js
@@ -21,7 +21,7 @@ function createCardElement(element) {
 function selectCard(element, cardElement) {
   // Add the selected element to the selectedCards array
   selectedCards.push(element);
-  //
+  //sdasdas dsadas
 
   // Visually indicate that the card has been selected
   cardElement.classList.add("selected", "glowing");
